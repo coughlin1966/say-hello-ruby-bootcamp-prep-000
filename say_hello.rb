9da87180ff360bb7b2b12731400873afbacabad2
Peter Coughlin <coughlin1966@gmail.com>
2018-01-26 18:16:39 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
-def say_hello (name, language="Ruby")
-  puts "Hello"
+def say_hello (name = "Ruby Programmer")
+  puts "Hello #{name}!"
 end
 say_hello("Kent Beck")
